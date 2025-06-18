736c1445c9cc574f235cc22a3b0075b3037742c1
LucSEILER <lucky200331a@gmail.com>
2025-06-18 12:16:12 +0200

    first commit

---
+++ api-gateway/src/server.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import { setupProxies } from './proxy';
+import { ROUTES } from './routes';
+
+dotenv.config()
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('/ of API Gateway');
+})
+
+setupProxies(app, ROUTES);
+
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack); 
+  res.status(500).send('Something broke!'); 
+});
+
+app.listen(port, () => {
+    console.log(`API Gateway is running at http://localhost:${port}`);
+});
