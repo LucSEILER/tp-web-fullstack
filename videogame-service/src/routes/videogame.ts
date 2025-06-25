52d70419d1ec70ce2c136f62c4a6800f8cc52e1b
LucSEILER <lucky200331a@gmail.com>
2025-06-25 08:31:12 +0200

    feat: Add game search and find by ID (#6)

--- videogame-service/src/routes/videogame.ts
+++ videogame-service/src/routes/videogame.ts
@@ -4,5 +4,7 @@ import videogameController from '../controllers/videogame'
 const videogameRoutes = express.Router()
 
 videogameRoutes.get('/', videogameController.getGames)
+videogameRoutes.get('/search', videogameController.searchGamesByName)
+videogameRoutes.get('/:id', videogameController.getGameById)
 
 export { videogameRoutes }
