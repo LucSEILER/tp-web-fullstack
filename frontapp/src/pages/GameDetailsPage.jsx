8ace021fbea9cb0a40da2f9c08b27ff5ee81527e
LucSEILER <lucky200331a@gmail.com>
2025-06-27 15:33:18 +0200

    feat: Make review system: User can now add a review with a note to a game

--- frontapp/src/pages/GameDetailsPage.jsx
+++ frontapp/src/pages/GameDetailsPage.jsx
@@ -3,11 +3,16 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { toaster } from "../components/ui/toaster";
 import GameDetails from "../components/organisms/GameDetails";
+import { Input } from "@chakra-ui/react";
+import Button from "../containers/atoms/Button";
 
 const GameDetailsPage = () => {
   const { id } = useParams();
   const [game, setGame] = useState(null);
   const [message, setMessage] = useState("");
+  const [gameReviews, setGameReviews] = useState([]);
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
 
   useEffect(() => {
     const fetchGame = async () => {
@@ -35,10 +40,96 @@ const GameDetailsPage = () => {
     fetchGame();
   }, [id]);
 
+  useEffect(() => {
+    const fetchGameReviews = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/videogame/games/reviews/${id}`,
+          { withCredentials: true }
+        );
+        const { data, message } = response.data;
+        console.log(response);
+
+        setMessage(message);
+        setGameReviews(data);
+
+        toaster.create({
+          type: "info",
+          description: message,
+          duration: 3000,
+        });
+      } catch (err) {
+        console.error("Failed to fetch game", err);
+      }
+    };
+
+    fetchGameReviews();
+  }, [id]);
+
+  const handleAddReview = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:4000/videogame/games/review`,
+        { rating, review, gameId: id, name: 'test' },
+        { withCredentials: true }
+      );
+      const { data, message } = response.data;
+      console.log(response);
+
+      setMessage(message);
+      // setGameReviews(data);
+
+      toaster.create({
+        type: "success",
+        description: message,
+        duration: 3000,
+      });
+
+      // call fetchGameReviews()
+    } catch (err) {
+      console.error("Failed to fetch game", err);
+    }
+  };
+
   if (!game) return <p>Loading...</p>;
 
   return (
-    <GameDetails game={game} />
+    <div>
+      <GameDetails game={game} />
+
+      <h2>Reviews</h2>
+
+      {!gameReviews ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {gameReviews.map((review) => (
+            <div key={review.id}>
+              <p>{review.user_id}</p>
+              <p>{review.review}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div>
+        <p>add reviews</p>
+
+        <Input
+          type="number"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <Input
+          type="text"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        />
+      </div>
+
+      <Button backgroundColor="buttonPrimary" onClick={handleAddReview} label="Add Review"/>
+    </div>
   );
 };
 
