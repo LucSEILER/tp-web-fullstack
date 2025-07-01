cb8760b14b6ca4bd7866e4980492591b429f0179
LucSEILER <lucky200331a@gmail.com>
2025-07-01 13:12:30 +0200

    refacto: I don't remember what

--- videogame-service/src/middleware/authToken.ts
+++ videogame-service/src/middleware/authToken.ts
@@ -3,32 +3,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// const authMiddleware = (req: any, res: any, next: any) => {
-//   const authHeaders = req.headers.authorization
-//   if (!authHeaders || !authHeaders.startsWith('Bearer ')) {
-//     return res.status(401).json({ message: 'Missing bearer token' })
-//   }
-
-//   const tokenFromHeader = authHeaders.split(' ')[1]
-//   const tokenFromCookie = req.cookies.idToken
-//   const token = tokenFromHeader || tokenFromCookie
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Missing token' })
-//   }
-
-//   console.log('Token:', token)
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET || '')
-//     req.user = decoded
-//     console.log('Decoded token:', decoded)
-//     next()
-//   } catch (error: any) {
-//     return res.status(401).json({ message: 'Unauthorized', error: error.message })
-//   }
-// }
-
 interface TokenResponse {
   token: string
   user: {
